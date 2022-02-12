8696c908b8178f9348ca764b82a38c94989df361
antor10655gh <creativefreelancerantor@gmail.com>
2022-02-13 00:21:05 +0600

    add app.js file and product update

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-console.log('I am in a separate file')
+function productQuantityUpdate(isIncreasing){
+    const productInput = document.getElementById('product-quantity');
+    const productQuantity = productInput.value;
+    if(isIncreasing == true){
+        productInput.value = parseInt(productQuantity) + 1;
+    }
+
+    else if(productQuantity > 0){
+        productInput.value = parseInt(productQuantity) - 1;
+    }
+
+    // product-price update
+    const productPrice = document.getElementById('product-price');
+    productPrice.innerText = productInput.value * 60;
+
+}
+
+document.getElementById('plus-btn').addEventListener('click', function(){
+       productQuantityUpdate(true);  
+})
+document.getElementById('minus-btn').addEventListener('click', function(){
+       productQuantityUpdate(false);
+})
