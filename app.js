9ccd5d5abc53f594fcbc171503cffcbb2913d5ac
antor10655gh <creativefreelancerantor@gmail.com>
2022-02-13 11:22:49 +0600

    add subtotal,tax,total price

--- app.js
+++ app.js
@@ -14,8 +14,31 @@ function pQuantityUpdate(product, isIncreasing, price){
     const pPrice = document.getElementById(product + '-price');
     pPrice.innerText = pInput.value * price;
 
+    // call getTotalValue() function
+    getTotalValue();
+
 }
 
+function getInputValue(product){
+    const pInput = document.getElementById(product + '-quantity');
+    const pInputValue = parseInt(pInput.value);
+    return pInputValue;
+}
+
+function getTotalValue(){
+    const phonePrice = getInputValue('phone') * 1220;
+    const productPrice = getInputValue('product') * 60;
+    const subTotal = phonePrice + productPrice;
+    const tax = subTotal * .1;
+    const total = subTotal + tax;
+
+    // update html
+    document.getElementById('subtotal-amount').innerText = subTotal;
+    document.getElementById('tax-amount').innerText = tax;
+    document.getElementById('total-amount').innerText = total;
+}
+ 
+// phone code is here
 document.getElementById('phone-plus-btn').addEventListener('click', function(){
     pQuantityUpdate('phone',true,1220);  
 })
@@ -23,6 +46,7 @@ document.getElementById('phone-minus-btn').addEventListener('click', function(){
     pQuantityUpdate('phone',false,1220);
 })
 
+// product code is here
 document.getElementById('plus-btn').addEventListener('click', function(){
     pQuantityUpdate('product',true,60);  
 })
